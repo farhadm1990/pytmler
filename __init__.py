47f5af0a0d288a8d541717c41cee3d8a4716f9cc
farhadm1990 <zardbid@gmail.com>
2023-09-21 15:18:34 +0200

    init_update

---
+++ __init__.py
@@ -0,0 +1,11 @@
+""""
+htmler
+
+A smart and small package to build quick but shinny html reports in python.
+
+"""
+
+__version__ = "0.0.1"
+__author__ = "Farhad M. Panah"
+__credits__ = "University of Copenhagen, Department of Food Science"
+__author_email__ = "farhad@food.ku.dk"
