a7d3127e4bb34e3b4ba97de03261f84f288017b5
farhadm1990 <zardbid@gmail.com>
2023-09-21 15:02:58 +0200

    2nd

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+
+    name="htmler",
+    version="0.0.1",
+    url='https://github.com/farhadm1990/htmler',
+    author="Farhad M. Panah",
+    author_email="farhad@food.ku.dk",
+    packages=["report_maker"],
+    install_requires=["jinja2", "datetime", "io", "base64", "os", "pandas", "numpy", "requests", "BeautifulSoup"]
+
+)
+
+
+
