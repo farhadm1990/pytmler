91997d8524b73053dbb232e7155048773175eb82
farhadm1990 <zardbid@gmail.com>
2023-09-22 14:30:10 +0200

    setup3rd

--- setup.py
+++ setup.py
@@ -9,7 +9,16 @@ setup(
     author="Farhad M. Panah",
     author_email="farhad@food.ku.dk",
     packages=find_packages(),
-    install_requires=["jinja2", "datetime", "pandas", "numpy", "requests", "beautifulsoup4"]
+    install_requires=["jinja2", "datetime", "pandas", "numpy", "requests", "beautifulsoup4"],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU General Public Licence V3.0",
+        "Operating System :: OS Independent",
+    ]
 
 )
 
